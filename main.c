05b3b1a34c77eeea88da4d3eb425fc7551878279
fahrezaarsyam <fahreza.arsya.m@gmail.com>
2024-12-07 18:39:17 +0700

    Separated File Functions

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include "function.h"
 
-void add(float x, float y, double *result); 
-void subtract(float x, float y, double *result);
-void multiply(float x, float y, double *result);
-void division(float x, float y,double *result);
-void exponent(float x, float y,double *result);
-void clear(double *result);
-void operation(int input, float x, float y, double *result);
 
 int main() {
     int input;
@@ -63,68 +57,3 @@ int main() {
     return 0;
 }
 
-void operation(int input, float x, float y, double *result) {
-    switch (input) {
-        /* Addition */
-        case 1:
-            add(x, y, result);
-            break;
-        
-        /* Subtraction */
-        case 2:
-            subtract(x, y, result);
-            break;
-        
-        /* Multiplication */
-        case 3:
-            multiply(x, y, result);
-            break;
-
-        /* Division */
-        case 4:
-            division(x,y, result);
-            break;
-
-        /* Exponents */
-        case 5:
-            exponent(x,y, result);
-            break;
-        
-        /* Clear */
-        case 7:
-            clear(result);
-            break;
-        
-        default:
-            break;
-    }
-}
-
-void add(float x, float y, double *result) {
-    *result = x + y;
-}
-
-void subtract(float x, float y, double *result) {
-    *result = x - y;
-}
-
-void multiply(float x, float y, double *result) {
-    *result = x * y;
-}
-
-void division(float x, float y,double *result){
-    if (y == 0) {
-        printf("Cannot divide by 0\n");
-    }
-    else{
-        *result = x / y;
-    }
-}
-
-void exponent(float x, float y,double *result){
-    *result = pow(x,y);
-}
-
-void clear(double *result) {
-    *result = 0;
-}
